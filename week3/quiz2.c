2b5057affea7116a176bfe3fecd16bf978ac8185
LarissaCirino <148501644+LarissaCirino@users.noreply.github.com>
2023-10-27 23:31:20 +0000

    quiz2

---
+++ week3/quiz2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int x = 5;
+int main(){
+if (x > 10) {
+    printf("A");
+} else if (x < 10) { 
+    printf("B");
+} else {
+    printf("C");
+};
+
+return 0;
+
+}
+
+//int foo() { 
+//    return 5; }
+//printf("%d", foo());
